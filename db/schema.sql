86018dddbeb365f2d062b4caba8f674fd28b909e
Nuphawan <nuphawan@yahoo.com>
2020-02-06 11:31:50 -0600

    updated to burger

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE burger_db;
-USE burger_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	burger BOOLEAN DEFAULT false,
+	burger_name VARCHAR(30) NOT NULL, 
+	devoured BOOLEAN DEFAULT, 
 	PRIMARY KEY (id)
 );
