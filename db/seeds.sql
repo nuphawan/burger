24d97b2d8228d57e36150f6525f0b15e2256b352
Nuphawan <nuphawan@yahoo.com>
2020-02-05 20:50:45 -0600

    updated to burger

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Meeses');
+INSERT INTO burgers (name) VALUES ('Bobbi');
+INSERT INTO burgers (name, burger) VALUES ('Sylvester', true);
+INSERT INTO burgers (name, burger) VALUES ('Marilyn Manson', true);
+INSERT INTO burgers (name, burger) VALUES ('Joe Biden', true);
+INSERT INTO burgers (name) VALUES ('Owens');
